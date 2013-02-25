672d40d8d459df6c8eaef09cfeceef0354e2175f
yjqian <yjqian@yahoo-inc.com>
2013-02-25 04:48:52 -0800

    dynamic child mojit instances

--- mojits/FrameMojit/controller.server.js
+++ mojits/FrameMojit/controller.server.js
@@ -2,7 +2,26 @@ YUI.add('FrameMojit', function(Y, NAME) {
   Y.namespace('mojito.controllers')[NAME] = {
 
     index: function(actionContext) {
-        actionContext.composite.done({title: "Parent Frame"});
+    	var cfg = {children: {
+    			header: {
+    				type: "HeaderMojit",
+    				action: "index"
+    			},
+				body: {
+					type: "BodyMojit",
+					action: "index"
+				},
+				footer: {
+					type: "FooterMojit",
+					action: "index"
+				}
+    		}
+    	};
+    	
+        //actionContext.composite.done({title: "Parent Frame"});
+    	actionContext.composite.execute(cfg, function(data, meta) {
+    		actionContext.done(data, meta);
+    	});
     }
   };
 }, '0.0.1', {requires: ['mojito-composite-addon']});
